10d1278745ef5a686778bc3b7959dab53b01271c
Shoib007 <skhantanda@gmail.com>
2023-08-12 16:54:08 +0530

    Last but not the least Push, I'll write more code on this later

--- takeTestApp/views.py
+++ takeTestApp/views.py
@@ -1,16 +1,16 @@
-from datetime import datetime, timedelta
-from django.db.models import Q, F, FloatField
+from datetime import datetime, timedelta, date
+from django.db.models import Q, F
 from django.db.models.functions import ACos, Sin, Cos, Radians
 from django.http import JsonResponse, HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
-from .models import Registration, TestCenter
+from .models import TestCenter
 from .serializer import RegistrationSerializer, TestCenterSerializer
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def Home(request):
-    return HttpResponse("<h1> This is Home </h1>")
+    return HttpResponse("I'm home")
 
 
 @api_view(['POST'])
@@ -18,19 +18,27 @@ def find_available_test_centers(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
-            print(request.data)
             # Extract data from the serializer
             student_data = request.data
             student_date = student_data['test_date']
             student_time = student_data['test_timing']
-            student_duration = student_data['test_duration']
+            student_duration_str = student_data['test_duration']
             student_latitude = student_data['preferred_location_lat']
             student_longitude = student_data['preferred_location_long']
 
-            # Calculate the end time of the student's test
 
-            student_date = datetime.strptime(student_date, '%Y-%m-%d')
-            student_start_datetime = datetime.combine(student_date, student_time)
+            # Convert student_time to a timedelta object
+            student_time = datetime.strptime(student_time, "%H:%M:%H").time()
+            
+
+            # Parse student_duration_str into a timedelta object
+            try:
+                student_duration = timedelta(hours=float(student_duration_str))
+            except ValueError:
+                return JsonResponse({'detail': 'Invalid test_duration format.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Calculate the end time of the student's test
+            student_start_datetime = datetime.combine(student_date, datetime.min.time()) + student_time
             student_end_datetime = student_start_datetime + student_duration
 
             # Check for available test centers
